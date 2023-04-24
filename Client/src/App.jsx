60f205fad8e0eff64b68eb391d64ee1a5f89a125
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2023-04-24 20:47:20 +0530

    done with card details page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
-import { CardDetail } from "./component/componentExport";
-
+import Header from "./components/Header";
+import CardDetail from "./components/CardDetail/CardDetail";
 function App() {
   return (
     <div>
-      <Header />
-      <CardDetail />
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/card" element={<CardDetail />} />
+      </Routes>
     </div>
   );
 }
+export default App;
