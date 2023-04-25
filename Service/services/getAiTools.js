50493ce0b152ab10589d35a5558c86cb128facff
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-04-25 12:45:13 +0530

    Add: regex for getALLTools search query

--- Service/services/getAiTools.js
+++ Service/services/getAiTools.js
@@ -1,21 +1,21 @@
 const { aiToolsData } = require("../Data/aiToolsData");
 
 const getAllAIToolsService = (searchQuery) => {
-    let aiTools = aiToolsData;
+  let aiTools = aiToolsData;
 
-    if (searchQuery) {
-      const query = searchQuery.toLowerCase();
-      aiTools = aiTools.filter(tool => {
-        return (
-          tool.name.toLowerCase().includes(query) ||
-          tool.description.toLowerCase().includes(query) ||
-          tool.tags.some(tag => tag.toLowerCase().includes(query))
-        );
-      });
-    }
-  
-    return aiTools;
-  };
+  if (searchQuery) {
+    const regex = new RegExp(searchQuery, "i");
+    aiTools = aiTools.filter((tool) => {
+      return (
+        regex.test(tool.name) ||
+        regex.test(tool.description) ||
+        tool.tags.some((tag) => regex.test(tag))
+      );
+    });
+  }
+
+  return aiTools;
+};
 
 const getAiToolByIdService = (id) => {
   return aiToolsData.find((tool) => tool.id === id);
