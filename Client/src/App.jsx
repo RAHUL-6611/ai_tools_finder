8f2e172c70abb69303142c271db197ea4427e569
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-04-25 15:03:54 +0530

    Feat: add AI Too Card

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css';
 import Header from './components/Header';
+import AiToolCard from './components/AiToolCard';
+import AiToolData from './Data/AiToolData';
 
 const App = () => {
   return (
     <div>
       <Header />
       {/* Other Components Go here */}
+      {/* <AiToolCard tool={AiToolData[0]}/> */}
     </div>
   )
 }
